26ecd4c2ea2db00d7710e02f8feca0d9eb15e632
Jeff <jeffersonpereira138@gmail.com>
2023-10-01 20:54:34 -0300

    Create contato.php

---
+++ contato.php
@@ -0,0 +1,25 @@
+<?php 
+?>
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contato</title>
+</head>
+<body>
+<header>
+
+</header>
+
+<main>
+
+</main>
+
+<footer>
+
+</footer>
+</body>
+</html>
